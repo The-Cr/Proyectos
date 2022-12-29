dbdf4d55a7d630f383052c956354c79e33e9330a
Cris1 <cristian_vallejos@yahoo.com>
2022-12-29 11:40:02 -0300

    #se crea la funcion palindromos y las entradas

--- 8_Es_Palindromo/main.py
+++ 8_Es_Palindromo/main.py
@@ -11,3 +11,22 @@ También lo es malayalam.
 En el caso de  geeks no lo es.
 '''
 
+palabras = []
+
+print('Ingresa 5 palabras ')
+
+for i in range(1,5):
+    print(i,':')
+    palabras.append(input(''))
+
+
+def palindromos(lista):
+    for palabra in lista:
+        inv_palabras = ''.join(reversed(palabra))
+        if inv_palabras == palabra:
+            print(palabra,' es un palindromo')
+        else:
+            print(palabra,' no es in palindromo')
+
+palindromos(palabras)
+
