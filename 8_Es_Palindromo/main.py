69f182676f6575ac234e06f46b165bbeaef2e046
Cris1 <cristian_vallejos@yahoo.com>
2022-12-29 11:05:35 -0300

    #se cambio la logica de la solucion

---
+++ 8_Es_Palindromo/main.py
@@ -0,0 +1,13 @@
+''''
+Animamos a los usuarios a introducir cinco palabras.
+Después comprobamos cuáles son palabras palíndromas o no.
+
+¿Qué es un palíndromo? Es una palabra que podemos leer de la misma manera desde la izquierda a la derecha y viceversa.
+
+Ejemplo:
+
+madam es palíndromo.
+También lo es malayalam.
+En el caso de  geeks no lo es.
+'''
+
