a46b24779a586d298046cc089acca5dbaaed04f4
Cris1 <cristian_vallejos@yahoo.com>
2022-12-28 22:25:56 -0300

    #se agrega funcion menu

---
+++ 7_Adivina_el_Numero_Oculto/main.py
@@ -0,0 +1,35 @@
+''''
+Debemos de preguntar al usuario un número entre 1 y 50.
+
+Si añaden un número fuera de ese rango, 
+vamos a indicar con un error que anime a elegir un número dentro del rango adecuado..
+
+Si no acertamos el número oculto,
+preguntaremos al usuario si queremos seguir jugando, 
+introduciendo un nuevo número o queremos dejar de jugar.
+
+Finalmente, cuando el usuario acierta correctamente el número oculto, 
+mostramos un mensaje de enhorabuena y 
+mostramos el número de intentos que hemos utilizado para llegar a esta situación.
+'''
+from random import randint
+
+numero_oculto = randint(0,50)
+
+user_num = int(input('Ingresa un nuemro entre 1 - 50'))
+
+def menu():
+    resp = input('Deseas Continuar s/n\n')
+
+def Validador(user_num,num_oculto):
+    if user_num == num_oculto:
+        print('Adivinaste Felicidades')
+    else:
+        print('Fallaste\n')
+        
+
+if  1 <= user_num and user_num <= 50:
+    Validador(user_num,numero_oculto)
+else:
+    print('Valor fuera de rango')
+    
