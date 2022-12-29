8d4278893d89e82d6ebe4137942d3866ccf6240d
Cris1 <cristian_vallejos@yahoo.com>
2022-12-29 11:43:01 -0300

    #se escribe la descripcion

---
+++ 9_Calculadora_de_Propinas/main.py
@@ -0,0 +1,13 @@
+''''
+En este caso, nuestro objetivo es averiguar exactamente la cantidad de propina que 
+hay que proporcionar después de un servicio. En este caso, 
+hay que solicitar la factura total. 
+Con esto, aplicaremos la propina para el 18%, 20% y 25%.
+
+Ejemplo:
+
+Mensaje inicial: ¿Cuál es la factura total de hoy, por favor?
+Entrada: $55.87
+Salida: La propina aplicando el 18% is $10.06, que contabiliza un total de $65.93
+'''
+
