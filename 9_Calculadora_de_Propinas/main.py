2295f5f3def48be69c380e787e1c74ce7e71182f
Cris1 <cristian_vallejos@yahoo.com>
2022-12-29 15:42:41 -0300

    #se termina la funcion fact

--- 9_Calculadora_de_Propinas/main.py
+++ 9_Calculadora_de_Propinas/main.py
@@ -11,3 +11,21 @@ Entrada: $55.87
 Salida: La propina aplicando el 18% is $10.06, que contabiliza un total de $65.93
 '''
 
+factura = float(input('Cual es la factura total de hoy?\n'))
+
+def fact(num):
+    if num > 1 and num < 60:
+        propina = round((num*18)/100,2)
+        porc_prop = 18
+        total = propina + num
+    if num > 60 and num < 120:
+        propina = round((num*20)/100,2)
+        porc_prop = 20
+        total = propina + num
+    if num > 120:
+        propina = round((num*25)/100,2)
+        porc_prop = 25
+        total = propina + num
+    print('La propina aplicando el ',porc_prop,'% , es', propina ,", que contabiliza un total de $",round(total,2))
+
+fact(factura)
